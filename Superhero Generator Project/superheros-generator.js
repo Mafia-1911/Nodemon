d6c93378164e3d578455cd1dcce57377fc20ea4e
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-08-31 21:19:24 +0600

    import and using the npm module

---
+++ Superhero Generator Project/superheros-generator.js
@@ -0,0 +1,9 @@
+import superheroes from "superheroes"
+const name=superheroes.randomSuperhero();
+console.log(name);
+
+
+// const generate=()=>{
+//     console.log(sh.randomSuperhero());
+// }
+// generate();
